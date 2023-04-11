ac007d64c3983a73edc347b4a1fdfa6c1a9a1b0f
javier <jfernandes113@gmail.com>
2023-04-11 19:34:38 +0200

    first commit

---
+++ db/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs/promises');
+const path = require('path');
+const getPath = (file) => path.join(__dirname, `${file}.json`);
+
+const read = async (file) => {
+  try {
+    const content = JSON.parse(await fs.readFile(getPath(file)));
+    return {
+      ok: true,
+      content,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      content: error.message,
+    };
+  }
+};
+
+const write = async (file, content) => {
+  try {
+    await fs.writeFile(getPath(file), JSON.stringify(content));
+    return {
+      ok: true,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      error: error.message,
+    };
+  }
+};
+
+module.exports = {
+  read,
+  write,
+};
