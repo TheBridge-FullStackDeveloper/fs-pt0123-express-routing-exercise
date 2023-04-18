a0f7120f99f081c692ebb5912d982a073b5f19b1
Molpe <fran.mscr@gmail.com>
2023-04-18 11:11:49 +0200

    feat: added all routes to cover all products needs

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,57 @@
 const router = require('express').Router()
+const products = require('../queries/products')
 
-// Your code goes here!
+const clientResponse = ({ ok, content }, res, next) => {
+    if(!ok) return next({
+        statusCode: 500,
+        error: new Error('something went wrong!')
+    })
+
+    res.status(200).json({
+        success: true,
+        data: content,
+    })
+}
+
+router.get('/', async (req, res, next) => {
+    const store = await products.selectAll()
+    clientResponse(store, res, next)
+})
+
+router.get('/stock', async (req, res, next) => {
+    const store = await products.selectManyByStock(req.query.max)
+    clientResponse(store, res, next)
+})
+
+router.get('/category', async (req, res, next) => {
+    const store = await products.selectManyByCategory(req.query.name)
+    clientResponse(store, res, next)
+})
+
+router.get('/sales', async (req, res, next) => {
+    const { limit } = req.query
+    const store = await products.sortBySales({ limit })
+    clientResponse(store, res, next)
+})
+
+router.get('/:id', async (req, res, next) => {
+    const store = await products.selectOneById(req.params.id)
+    clientResponse(store, res, next)
+})
+
+router.post('/', async (req, res, next) => {
+    const store = await products.insertOne(req.body)
+    clientResponse(store, res, next)
+})
+
+router.put('/:id', async (req, res, next) => {
+    const store = await products.updateOne(req.params.id, req.body)
+    clientResponse(store, res, next)
+})
+
+router.delete('/:id', async (req, res, next) => {
+    const store = await products.deleteOne(req.params.id)
+    clientResponse(store, res, next)
+})
 
 module.exports = router
